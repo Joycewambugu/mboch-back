6ecc26fbf6349c52986a3fec683162ec408c14c2
Morris Mukiri <morrismukiri@gmail.com>
2017-11-22 15:31:11 +0300

    Api resource name

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::resource('subscription_plans', 'SubscriptionPlanAPIController');
+Route::apiResource('subscriptionPlans', 'API\SubscriptionPlanAPIController');
