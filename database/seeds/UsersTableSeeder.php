fc947648a730a38e47b33eca7c12355d38461a81
Morris Mukiri <morrismukiri@gmail.com>
2017-12-02 14:15:04 +0300

    Seeed the superuser

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,15 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->where('email', 'morrismukiri@hotmail.com')->delete();
+        DB::table('users')->insert([
+            'name' => "Morris Mukiri",
+            'email' => "morrismukiri@hotmail.com",
+            'phone' => "+254716043576",
+            'password' => bcrypt('password123'),
+            'created_at'=>date("Y-m-d H:i:s"),
+            'updated_at'=>date("Y-m-d H:i:s")
+        ]);
         factory(\App\User::class, 10)->create();
     }
 }
