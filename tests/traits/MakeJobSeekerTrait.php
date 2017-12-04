47bea922744d2bf42a3681f53b2a9815f02085be
Morris Mukiri <morrismukiri@gmail.com>
2017-12-04 21:20:34 +0300

    use same faker data from factory in tests

--- tests/traits/MakeJobSeekerTrait.php
+++ tests/traits/MakeJobSeekerTrait.php
@@ -39,26 +39,25 @@ trait MakeJobSeekerTrait
      */
     public function fakeJobSeekerData($jobSeekerFields = [])
     {
-        $fake = Faker::create();
+        $faker = Faker::create();
         return array_merge([
-            'name' => $fake->word,
-            'email' => $fake->word,
-            'phone' => $fake->word,
-            'date_of_birth' => $fake->date,
-            'gender' => $fake->word,
-            'education_level' => $fake->word,
-            'tribe' => $fake->word,
-            'photo' => $fake->word,
-            'national_id' => $fake->word,
-            'experience_years' => $fake->randomDigitNotNull,
-            'spoken_languages' => $fake->word,
-            'religion' => $fake->word,
-            'employment_status' => $fake->word,
-            'marital_status' => $fake->word,
-            'max_children' => $fake->randomDigitNotNull,
-            'health_conditions' => $fake->text,
-            // 'created_at' => $fake->word,
-            // 'updated_at' => $fake->word
+            'name' => $faker->name,
+            'email' => $faker->unique()->email,
+            'phone' => $faker->unique()->PhoneNumber,
+            'date_of_birth' => $faker->date,
+            'gender' => $faker->randomElement($array = array ('male', 'female')),
+            'education_level' =>  $faker->randomElement($array = array ('primary', 'secondary','college','university')),
+            'current_location' =>  $faker->city,
+            'tribe' =>  $faker->randomElement($array = array ('Dahalo','Digo','Duruma','Edo','El Molo','Embu','Garreh-Ajuran','Giryama','Kalenjin','Kamba','Kikuyu','Kisii','Kuria','Luhya','Luo','Masai','Meru','Mijikenda','Ogiek','Rendille','Samburu','Somali','Swahili','Taita','Teso','Tharaka','Turkana','Yaaku')),
+            'photo' => $faker->imageUrl($width = 640, $height = 480),
+            'national_id' => $faker->randomNumber(8),
+            'experience_years' => $faker->randomDigitNotNull,
+            'spoken_languages' => $faker->word,
+            'religion' => $faker->word,
+            'employment_status' =>  $faker->randomElement($array = array ('employed', 'searching','suspended')),
+            'marital_status' => $faker->randomElement($array = array ('single', 'married','divorced','widowed')),
+            'max_children' => $faker->randomDigitNotNull,
+            'health_conditions' => $faker->text,
         ], $jobSeekerFields);
     }
 }
