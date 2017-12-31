c02421ae3794f14c5cf341cdac3862b3782fba46
Morris Mukiri <morrismukiri@gmail.com>
2017-12-31 23:59:33 +0300

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,7 +38,7 @@
                 </div>
                 </div>
 <!-- multistep form -->
-<form id="msform">
+<form id="msform" >
   <!-- progressbar -->
   <ul id="progressbar">
     <li class="active">Account Setup</li>
@@ -98,16 +98,6 @@
     <input type="text" name="max_children" placeholder="max children" />
     <input type="text" name="health_conditions" placeholder="health conditions" />
 
-    <input type="button" name="previous" class="previous action-button" value="Previous" />
-    <input type="button" name="next" class="next action-button" value="Next" />
-  </fieldset>
-  <fieldset>
-    <h2 class="fs-title">Personal Details</h2>
-    <h3 class="fs-subtitle">We will never sell it</h3>
-    <input type="text" name="fname" placeholder="First Name" />
-    <input type="text" name="lname" placeholder="Last Name" />
-    <input type="text" name="phone" placeholder="Phone" />
-    <textarea name="address" placeholder="Address"></textarea>
     <input type="button" name="previous" class="previous action-button" value="Previous" />
     <input type="submit" name="submit" class="submit action-button" value="Submit" />
   </fieldset>
